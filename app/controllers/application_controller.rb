6f11f57d594481216ea8ab6c5323c951d710bba3
hectortllo <hectortllo@gmail.com>
2019-09-17 21:31:35 -0600

    Refactored application controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
-
-    #Ejecutar esto antes que cualquier otra cosa
     include DeviseWhitelist
-
-    before_action :set_source
-
-    def set_source
-        session[:source] = params[:q] if params[:q]
-    end
+    include SetSource
 end
