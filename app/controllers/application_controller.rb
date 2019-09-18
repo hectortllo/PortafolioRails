82f095936373109efceecad4a7568da48d02135c
hectortllo <hectortllo@gmail.com>
2019-09-17 19:55:50 -0600

    Added controller concerns for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
 
     #Ejecutar esto antes que cualquier otra cosa
-    before_action :configure_permitted_parameters, if: :devise_controller?
-
-    #Método para permitir el paso del parámetro del nombre en el registro de usuarios
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
-    
+    include DeviseWhitelist
 end
