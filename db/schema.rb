02900ed8e394f4e8f6b20c5c357e1e285c0e7c0b
Héctor Tello <hectortllo@gmail.com>
2021-01-24 15:58:22 -0600

    Se actualizó la información del contact y about

--- db/schema.rb
+++ db/schema.rb
@@ -59,13 +59,6 @@ ActiveRecord::Schema.define(version: 2019_09_25_230133) do
     t.integer "position"
   end
 
-  create_table "sells", force: :cascade do |t|
-    t.date "date_sell"
-    t.float "total"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
